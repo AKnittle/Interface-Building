670c01462ad886a81638ea23b70563414d60f840
Andrew Knittle <andrewknittle@andrews-macbook-pro.local>
2016-08-09 13:29:21 -0400

    Initial Commit

---
+++ Interface Builder/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Interface Builder
+//
+//  Created by Andrew Knittle on 8/9/16.
+//  Copyright © 2016 Andrew Knittle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
